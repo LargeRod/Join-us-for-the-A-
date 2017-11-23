dff4501105a6dbabe78baf8ad4209377a33bcb95
Emilie Vockeroth <emvock@gmail.com>
2017-11-23 12:49:09 -0500

    added the fragment folder as a new project as a test

---
+++ ChoreManagerFragments/app/src/main/java/joinusforthea/choreproject/choremanager11/ScheduleActivity.java
@@ -0,0 +1,25 @@
+package joinusforthea.choreproject.choremanager11;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.CalendarView;
+import android.widget.Toast;
+
+public class ScheduleActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_schedule);
+
+        CalendarView calendarView = (CalendarView) findViewById(R.id.view_calendar);
+        calendarView.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
+            @Override
+            public void onSelectedDayChange(@NonNull CalendarView view, int year, int month, int dayOfMonth) {
+                Toast.makeText(getApplicationContext(), dayOfMonth+"/"+month+"/"+year, Toast.LENGTH_LONG);
+            }
+        });
+
+    }
+}
