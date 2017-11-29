b471097951b3118fd40e1cc36b4ea948c85e96e3
Mitchell Elbaz <m-elbaz@Mitchell-Elbaz.local>
2017-11-28 21:39:49 -0500

    created java class for the custom chore layout

---
+++ ChoreManagerFragments/app/src/main/java/joinusforthea/choreproject/choremanager11/OpenedTaskActivity.java
@@ -0,0 +1,18 @@
+package joinusforthea.choreproject.choremanager11;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+/**
+ * Created by m-elbaz on 2017-11-28.
+ */
+
+public class OpenedTaskActivity extends AppCompatActivity{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile_page);
+    }
+}
