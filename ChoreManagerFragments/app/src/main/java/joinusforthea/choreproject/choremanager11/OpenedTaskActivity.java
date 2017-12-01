a73d61f7144b763e4727fc2f5fba0705ad8a3ac7
Mitchell Elbaz <m-elbaz@Mitchell-Elbaz.local>
2017-11-30 22:29:48 -0500

    added scroll wheel for date but now taskOpenedFragment wont open and crashes the app

--- ChoreManagerFragments/app/src/main/java/joinusforthea/choreproject/choremanager11/OpenedTaskActivity.java
+++ ChoreManagerFragments/app/src/main/java/joinusforthea/choreproject/choremanager11/OpenedTaskActivity.java
@@ -1,8 +1,17 @@
 package joinusforthea.choreproject.choremanager11;
 
+import android.app.DatePickerDialog;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.DatePicker;
+import android.widget.TextView;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Locale;
+
 
 /**
  * Created by m-elbaz on 2017-11-28.
@@ -14,5 +23,44 @@ public class OpenedTaskActivity extends AppCompatActivity{
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_opened_task);
+
+
+        textView.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick (View v) {
+                // TODO Auto-generated method stub
+                new DatePickerDialog(OpenedTaskActivity.this, date, myCalendar
+                        .get(Calendar.YEAR), myCalendar.get(Calendar.MONTH),
+                        myCalendar.get(Calendar.DAY_OF_MONTH)).show();
+            }
+        });
+
+
+
+    }
+
+    Calendar myCalendar = Calendar.getInstance();
+
+    TextView textView = (TextView) findViewById(R.id.dueDateText);
+    DatePickerDialog.OnDateSetListener date = new DatePickerDialog.OnDateSetListener() {
+
+        @Override
+        public void onDateSet(DatePicker view, int year, int monthOfYear,
+                              int dayOfMonth) {
+            // TODO Auto-generated method stub
+            myCalendar.set(Calendar.YEAR, year);
+            myCalendar.set(Calendar.MONTH, monthOfYear);
+            myCalendar.set(Calendar.DAY_OF_MONTH, dayOfMonth);
+            updateLabel();
+        }
+
+    };
+
+    private void updateLabel() {
+        String myFormat = "MM/dd/yy"; //In which you need put here
+        SimpleDateFormat sdf = new SimpleDateFormat(myFormat, Locale.US);
+
+        textView.setText(sdf.format(myCalendar.getTime()));
     }
 }
--- ChoreManagerFragments/app/src/main/java/joinusforthea/choreproject/choremanager11/SettingsActivity.java
+++ ChoreManagerFragments/app/src/main/java/joinusforthea/choreproject/choremanager11/SettingsActivity.java
@@ -2,11 +2,8 @@ package joinusforthea.choreproject.choremanager11;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-<<<<<<< HEAD
-=======
 import android.view.View;
 import android.widget.ListView;
->>>>>>> b5a4411a54337629c1fad0ea248a19398618c4cf
 
 public class SettingsActivity extends AppCompatActivity {
 
@@ -15,8 +12,6 @@ public class SettingsActivity extends AppCompatActivity {
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
-<<<<<<< HEAD
-=======
 
     }
 
@@ -35,6 +30,5 @@ public class SettingsActivity extends AppCompatActivity {
         //IMPLEMENT DELETING A USER IN HERE
 
         //SHOULD CAUSE ALL THEIR TASKS TO BECOME "UNASSIGNED"
->>>>>>> b5a4411a54337629c1fad0ea248a19398618c4cf
     }
 }
