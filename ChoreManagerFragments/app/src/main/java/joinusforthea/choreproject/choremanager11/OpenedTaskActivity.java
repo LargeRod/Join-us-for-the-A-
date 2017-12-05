7f68491e0799c982ff5a3af46f7830b786fc7c09
Mitchell Elbaz <m-elbaz@Mitchell-Elbaz.local>
2017-12-04 21:29:04 -0500

    added calendar picked for the due date of a task

--- ChoreManagerFragments/app/src/main/java/joinusforthea/choreproject/choremanager11/OpenedTaskActivity.java
+++ ChoreManagerFragments/app/src/main/java/joinusforthea/choreproject/choremanager11/OpenedTaskActivity.java
@@ -1,13 +1,17 @@
 package joinusforthea.choreproject.choremanager11;
 
+import android.app.DialogFragment;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.KeyEvent;
+import android.view.View;
+import android.widget.TextView;
 
 /**
  * Created by m-elbaz on 2017-11-28.
  */
 
-public class OpenedTaskActivity extends AppCompatActivity{
+public class OpenedTaskActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,4 +20,20 @@ public class OpenedTaskActivity extends AppCompatActivity{
         setTitle(taskName);
         setContentView(R.layout.activity_opened_task);
     }
+
+    //calling the DatePickerFragment to display a date picker dialog
+    public void showDatePickerDialog(View v) {
+
+        DialogFragment dateFragment = new DatePickerFragment();
+        dateFragment.show(getFragmentManager(), "datePicker");
+
+        //MUST OBTAIN DATE FROM DATEPICKERFRAGMENT AND CALL SETDATE WITH IT TO SET
+        //IT IN THE VIEW
+    }
+
+    public void setDate(String date) {
+        TextView dateText = (TextView) findViewById(R.id.dateText);
+        dateText.setText(date);
+    }
+
 }
