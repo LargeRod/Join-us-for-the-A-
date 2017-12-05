e923b3e3b452d9b82754c7abaf4c135fcb70eb32
melbaz98 <melba077@uottawa.ca>
2017-12-05 16:57:23 -0500

    remapped schedule to the right activity when creating a task

--- ChoreManagerFragments/app/src/main/java/joinusforthea/choreproject/choremanager11/ScheduleActivity.java
+++ ChoreManagerFragments/app/src/main/java/joinusforthea/choreproject/choremanager11/ScheduleActivity.java
@@ -5,7 +5,9 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.Button;
 import android.widget.CalendarView;
+import android.widget.EditText;
 import android.widget.Toast;
 
 public class ScheduleActivity extends AppCompatActivity {
@@ -23,10 +25,22 @@ public class ScheduleActivity extends AppCompatActivity {
             }
         });
 
+        final EditText taskEditText = (EditText) findViewById(R.id.newTaskText);
+        final Button addTaskButton = (Button) findViewById(R.id.newTaskButton);
+
+        addTaskButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (!taskEditText.getText().toString().equals("")) {
+                    String taskName = taskEditText.getText().toString();
+                    Intent intent = new Intent(ScheduleActivity.this, TaskAddActivity.class);
+                    startActivity(intent);
+                } else {
+                    Toast.makeText(ScheduleActivity.this, "Please enter a new task name", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
     }
 
-    public void openTaskActivity (View v) {
-        Intent intent = new Intent(ScheduleActivity.this, OpenedTaskActivity.class);
-        startActivity(intent);
-    }
 }
