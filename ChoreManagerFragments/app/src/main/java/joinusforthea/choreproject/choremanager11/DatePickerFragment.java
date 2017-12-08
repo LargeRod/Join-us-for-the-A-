82c579c0c8277b3ad9ef2e9d2a043889aca56ff2
Emilie Vockeroth <emvock@gmail.com>
2017-12-07 20:41:52 -0500

    im not sure but I didnt break anything so im pushing

--- ChoreManagerFragments/app/src/main/java/joinusforthea/choreproject/choremanager11/DatePickerFragment.java
+++ ChoreManagerFragments/app/src/main/java/joinusforthea/choreproject/choremanager11/DatePickerFragment.java
@@ -3,6 +3,7 @@ package joinusforthea.choreproject.choremanager11;
 import android.app.DatePickerDialog;
 import android.app.Dialog;
 import android.app.DialogFragment;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.widget.DatePicker;
 import android.widget.Toast;
@@ -52,6 +53,42 @@ public class DatePickerFragment extends DialogFragment implements DatePickerDial
         String chosenMonth = months[month-1];
         date = chosenMonth+" "+day+", "+year;
         Toast.makeText(getActivity(), "Date from datePickerFragment: " + this.toString(), Toast.LENGTH_LONG).show();
+
+
+
+    }
+
+    public void onStart() {
+        super.onStart();
+        // Use the current date as the default date in the picker
+        final Calendar c = Calendar.getInstance();
+        int year = c.get(Calendar.YEAR);
+        int month = c.get(Calendar.MONTH);
+        int day = c.get(Calendar.DAY_OF_MONTH);
+        long millis = Long.valueOf(0);
+
+        // Create a new instance of DatePickerDialog
+        DatePickerDialog dpdialog = new DatePickerDialog(getActivity(), this, year, month, day);
+
+        //convert date to long
+        SimpleDateFormat f = new SimpleDateFormat("dd-MMM-yyyy");
+        String stringDate = day+"-"+months[month]+"-"+year;
+        try {
+            Date d = f.parse(stringDate);
+            millis = d.getTime();
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+
+        //dont allow earlier dates than it currently is
+        dpdialog.getDatePicker().setMinDate(millis);
+
+        //returns the dialog to the activity
+    }
+
+    @Override
+    public void onCancel(DialogInterface dialog) {
+        super.onCancel(dialog);
 
     }
 
